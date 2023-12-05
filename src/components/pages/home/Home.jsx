0a6d19977bc9036a1fe471defeb72b0647c076c8
Jobayer-Ahmmed <jobayer.ahmmed.jas@gmail.com>
2023-12-05 21:37:30 +0600

    suport component has been deleted

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,23 +1,20 @@
 import Banner from "./banner/Banner"
 import Categories from "./category/Categories"
 import Employees from "./employee/Employees"
-import Support from "./support/Support"
+
 
 
 
 
 const Home = () => {
 
-  // const absuloteCategory=(a)=>{
-
-  // }
-
+  
   return (
     <div>
       <Banner/>
       <Categories/>
       <Employees/>
-      <Support/>
+
     </div>
   )
 }
